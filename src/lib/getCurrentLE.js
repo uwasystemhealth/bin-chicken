7cce42410a77bcaf667a0af37834151a87b733a0
Frinze Erin Lapuz <44391389+frinzekt@users.noreply.github.com>
2021-01-26 02:01:22 +0800

    handle the undefined `valid` variable certificate

--- src/lib/getCurrentLE.js
+++ src/lib/getCurrentLE.js
@@ -14,10 +14,16 @@ module.exports = async () => {
       certs[v] = { name: v };
     } else if(i === 'Expiry Date') {
       const [timestamp, valid] = v.split(' (VALID:');
-      certs[current].expiry = {
-        timestamp: new Date(timestamp),
-        valid: valid.replace(/[)]/g, '').trim(),
-      };
+      
+      // If the certificate is not undefined from the result of the split
+      // add to available certificates that are that can be used
+      // For more information about invalid certificates, examine your certbot configuration
+      if(!!valid){
+        certs[current].expiry = {
+          timestamp: new Date(timestamp),
+          valid: valid.replace(/[)]/g, '').trim(),
+        };
+      }
     } else {
       certs[current][i.toLocaleLowerCase().replace(/\s/g, '_')] = v.trim();
     }
